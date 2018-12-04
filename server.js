db139cb263edd20102171969ba7e47f4305c70c2
richcoleman <coleman.richardm@gmail.com>
2018-12-03 21:45:43 -0600

    adding server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+// setup middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+require("./app/routing/htmlRoutes")(app);
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+    console.log(`Server listening on: http://localhost:` + PORT);
+});
