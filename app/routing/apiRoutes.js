a9a244809cb47a94dd09f9e76115d368679ed4be
richcoleman <coleman.richardm@gmail.com>
2018-12-08 16:54:48 -0600

    added more edits

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
 let friendsObj = require("../data/friends.js");
 
 module.exports = function (app) {
-    require("../data/friends.js");
     app.get("/api/friends", function (req, res) {
         return res.json(friendsObj)
     });
 
     app.post("/api/friends", function (req, res) {
         let newFriend = req.body;
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        console.log("New Friend info:")
         console.log(newFriend);
-        res.json(newFriend);
+        return res.json(newFriend);
     })
 
 };
--- server.js
+++ server.js
@@ -1,12 +1,41 @@
 const express = require("express");
 const PORT = process.env.PORT || 3000;
+const path = require("path");
 const app = express();
-require("./app/routing/htmlRoutes")(app);
-require("./app/routing/apiRoutes")(app);
+let friendsObj = require("./app/data/friends");
+// require("./app/routing/apiRoutes")(app);
 
 // setup middleware
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
+
+// HTML routes
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, `app/public/home.html`));
+});
+
+// API Routes
+app.get("/api/friends", function (req, res) {
+    return res.json(friendsObj)
+});
+
+app.post("/api/friends", function (req, res) {
+    let newFriend = req.body;
+    // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+    console.log("New Friend info:")
+    console.log(newFriend);
+    friendsObj.push(newFriend)
+    res.json(newFriend);
+})
+
+
+
 
 
 // setup listener
